ed2867a2bf84d4c23443926a17d234d25c45a16b
ADMHWalker <andrew.walker@mass.gov>
2025-10-28 09:38:55 -0400

    Folder created for DART queries, with query bt client name.

---
+++ ClientInformationRequests/SQL_Script_Collection/DART_Queries/DART_Qry_Client_Name.sql
@@ -0,0 +1,46 @@
+/*
+Simple Client Name Query for DART
+Need to chek for mid init.
+ 
+*/
+USE DART
+Declare @NameString as varchar(50)
+Declare @Lname as varchar(30)
+Declare @Fname as varchar(30)
+Declare @LnamePat as varchar(30)
+Declare @NameParse as int
+--SELECT @NameString = 'SMITH POWSNER,JONAH L'
+--SELECT @NameString = 'Whitmer,Finn'
+SELECT @NameString = 'Wenckus,Aidan'
+--SELECT @NameString = 'Aidan,Wenckus'
+
+SELECT @NameParse =Patindex('%,%',@NameString)
+select @Lname = Left(@NameString,@NameParse-1)
+select @Fname = Right(@NameString,Len(@NameString) - @NameParse)
+select @LnamePat = RTRIM(@Lname) + '%'
+
+select @Lname, @Fname, @LnamePat
+--SELECT @Lname = RTRIM(@Lname) + '%'; 
+
+SELECT * FROM  tbl_REFERRAL
+WHERE (NAME LIKE @Lname)
+
+SELECT * FROM  tbl_REFERRAL
+WHERE (NAME LIKE @LnamePat)
+ORDER by NAME
+
+--SELECT CLIENT_ID, NAME, DOB, SSN, DECEASED, DATE_OF_DEATH, ADDRESS_STREET, ADDRESS_LINE_2, ADDRESS_CITY, ADDRESS_STATE, ADDRESS_ZIP, PHONE, GENDER, RACE_ID, RELIGION, ETHNIC_GROUP, US_CITIZEN, GUARDIANSHIP, VETERAN_STATUS, 
+--             EMPLOY_STATUS, MARITAL_STATUS, DEPENDANT_MINORS, PREF_LANGUAGE, NEED_INTERPRETER, LIVING_ARR, EDUC_STATUS, STILL_IN_SCHOOL, CUSTODY_STATUS, DMH_Area, DMH_Site, SOURCE_HCIS, SOURCE_PATIENT_ID, ROW_CREATE_DATE, Reg_Voter, Area, 
+--             MHIS_Area_ID, MHIS_Site_ID
+--FROM  tbl_REFERRAL
+--WHERE (NAME LIKE @Lname)
+
+--SELECT CLIENT_ID, NAME, DOB, SSN, DECEASED, DATE_OF_DEATH, ADDRESS_STREET, ADDRESS_LINE_2, ADDRESS_CITY, ADDRESS_STATE, ADDRESS_ZIP, PHONE, GENDER, RACE_ID, RELIGION, ETHNIC_GROUP, US_CITIZEN, GUARDIANSHIP, VETERAN_STATUS, 
+--             EMPLOY_STATUS, MARITAL_STATUS, DEPENDANT_MINORS, PREF_LANGUAGE, NEED_INTERPRETER, LIVING_ARR, EDUC_STATUS, STILL_IN_SCHOOL, CUSTODY_STATUS, DMH_Area, DMH_Site, SOURCE_HCIS, SOURCE_PATIENT_ID, ROW_CREATE_DATE, Reg_Voter, Area, 
+--             MHIS_Area_ID, MHIS_Site_ID
+--FROM  tbl_REFERRAL
+--WHERE (NAME LIKE @LnamePat)
+--ORDER by NAME
+
+--WHERE (NAME LIKE 'Whitmer%')
+
